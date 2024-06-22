9c2fc208572c9c1f3698bfb91e82eeff71ade2c6
pradumkumar6 <pradumjaiswar790@gmail.com>
2024-06-22 18:06:08 +0530

    resolve some issue

--- db.js
+++ db.js
@@ -1,20 +1,22 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
-// const mongoURL = "mongodb://127.0.0.1:27017/restaurantsApp";
+
 const mongoURL = process.env.MONGO_URL;
-mongoose.connect(mongoURL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+
+mongoose.connect(mongoURL);
+
 const db = mongoose.connection;
 
 db.on("connected", () => {
   console.log("Connected to MongoDB Server.");
 });
+
 db.on("error", (err) => {
   console.log("MongoDB connection error.", err);
 });
+
 db.on("disconnected", () => {
   console.log("MongoDB disconnected.");
 });
+
 module.exports = db;
